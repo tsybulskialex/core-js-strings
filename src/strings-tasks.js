fe01358e8f9d1f09870273e84660f2f41f16e096
Aleksandr <tsybulskialex@gmail.com>
2024-11-03 14:28:12 +0300

    feat: 35 tasks completed

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -273,8 +273,16 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  let min = String(minutes);
+  let sec = String(seconds);
+  if (+minutes < 10) {
+    min = min.padStart(2, '0');
+  }
+  if (+seconds < 10) {
+    sec = sec.padStart(2, '0');
+  }
+  return `${min}:${sec}`;
 }
 
 /**
@@ -367,8 +375,20 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let result = '';
+  let newStr = '';
+  const signs = ',!-?. ';
+  for (let i = 0; i < str.length; i += 1) {
+    if (!signs.includes(str[i]) || !signs.includes(str[i])) {
+      result = str[i] + result;
+      newStr += str[i];
+    }
+  }
+  if (newStr.toLowerCase() === result.toLowerCase()) {
+    return true;
+  }
+  return false;
 }
 
 /**
